bb47ce5d94f4c2d461c81ac8c085914672799f5e
Waamber <amberastuart@aol.com>
2017-08-28 18:47:55 -0500

    added json nd parsing

--- dogs.js
+++ dogs.js
@@ -1,1 +1,27 @@
-console.log("hello, buddy");
+console.log("YES, THIS IS DOG!");
+
+function loadDogs(){
+	var data = JSON.parse(this.responseText).dogs; 
+	console.log("dogs:", data);
+};
+
+function error() {
+	console.log("AHHHHH");
+};
+
+// function breeds(){
+// 	var breedsData = JSON.parse(this.responseText); 
+// 	console.log(breedsData);
+// };
+
+var dogRequest = new XMLHttpRequest();
+dogRequest.addEventListener("load", loadDogs);
+dogRequest.addEventListener("error", error);
+dogRequest.open("GET", "dogs.json");
+dogRequest.send();
+
+// var breedRequest = new XMLHttpRequest();
+// breedRequest.addEventListener("load", breeds);
+// breedRequest.addEventListener("error", error);
+// breedRequest.open("GET", "breeds.json");
+// breedRequest.send();
